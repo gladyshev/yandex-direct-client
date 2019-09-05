f7db9907bf2159a19be76a9147c101d5958305b3
Dmitry Gladyshev <deel@email.ru>
2019-09-05 13:28:38 +0300

    * Expand AgencyClients::add method parameters * Add AgencyClients::update mapping * Codestyle

--- src/Service/AgencyClients.php
+++ src/Service/AgencyClients.php
@@ -8,6 +8,7 @@ namespace Yandex\Direct\Service;
 
 use Yandex\Direct\Exception\Exception;
 use Yandex\Direct\Service;
+use Throwable;
 
 final class AgencyClients extends Service
 {
@@ -19,24 +20,19 @@ final class AgencyClients extends Service
      * @param $FieldNames
      * @param $Page
      * @return array
-     * @throws Exception
+     * @throws Throwable
      *
      * @see https://tech.yandex.ru/direct/doc/ref-v5/agencyclients/get-docpage
      */
     public function get($SelectionCriteria, $FieldNames, $Page = null)
     {
-        $params = [
-            'SelectionCriteria' => $SelectionCriteria,
-            'FieldNames' => $FieldNames
-        ];
-
-        if ($Page) {
-            $params['Page'] = $Page;
-        }
-
         return $this->request([
             'method' => 'get',
-            'params' => $params
+            'params' => array_filter([
+                'SelectionCriteria' => $SelectionCriteria,
+                'FieldNames' => $FieldNames,
+                'Page' => $Page
+            ])
         ]);
     }
 
@@ -44,17 +40,52 @@ final class AgencyClients extends Service
      * Регистрирует новых рекламодателей — клиентов агентства,
      * а также пользователей — главных представителей рекламодателя.
      *
-     * @param $AgencyClient
+     * @param $Login
+     * @param $FirstName
+     * @param $LastName
+     * @param $Currency
+     * @param $Grants
+     * @param $Notification
+     * @param $Settings
      * @return array
      * @throws Exception
+     * @throws Throwable
      *
      * @see https://tech.yandex.ru/direct/doc/ref-v5/agencyclients/add-docpage/
      */
-    public function add($AgencyClient)
+    public function add($Login, $FirstName, $LastName, $Currency, $Notification, $Grants = null, $Settings = null)
     {
         return $this->request([
             'method' => 'add',
-            'params' => $AgencyClient
+            'params' => array_filter([
+                'Login' => $Login,
+                'FirstName' => $FirstName,
+                'LastName' => $LastName,
+                'Currency' => $Currency,
+                'Grants' => $Grants,
+                'Notification' => $Notification,
+                'Setting' => $Settings
+            ])
+        ]);
+    }
+
+    /**
+     * Изменяет параметры рекламодателей — клиентов агентства, а также настройки пользователей —
+     * главных представителей рекламодателя.
+     *
+     * @inheritDoc
+     * @param $Clients
+     * @throws Throwable
+     *
+     * @see https://yandex.ru/dev/direct/doc/ref-v5/agencyclients/update-docpage/
+     */
+    public function update($Clients)
+    {
+        return $this->request([
+            'method' => 'update',
+            'params' => [
+                'Clients' => $Clients
+            ]
         ]);
     }
 }
